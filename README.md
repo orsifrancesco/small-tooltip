6856e8301a2362ab2ef06279501a7b1950ade395
Francesco Orsi <hello@orsi.me>
2020-06-13 23:52:47 +0100

    update

--- README.md
+++ README.md
@@ -14,13 +14,27 @@ $ npm install small-tooltip
 
 ```jsx
 
+  // just paste this 2 lines of code in your main file (index.js.. or app.js..)
+
   import smallTooltip from 'small-tooltip';
   import 'small-tooltip/smallTooltip.css';
+
+  // that's it, I will be always available in all the pages of your project!
+
+```
+
+```html
+  
+  <div data-tip="I'm a small tooltip">Move your mouse here to see me</div>
+
+```
+
+```jsx
 
   function App() {
 
     return (
-      <div data-tip="I'm a message">Move your mouse here to see me</div>
+      <div data-tip="I'm another small tooltip">Move your mouse here to see me as well!</div>
     );
 
   }
--- index.js
+++ index.js
@@ -1,60 +1,66 @@
 class smallTooltipClass {
 
-    constructor() {
+	constructor() {
 
-        this.injectHTML();
+		this.injectHTML();
 		this.allTipsSelector = "*[data-tip]";
-		this.allTips = document.querySelectorAll(this.allTipsSelector);
-        this.dom = document.querySelector("#small-tooltip");
-        this.events();
+		this.isIn = false;
+		this.lastText = '';
+		this.dom = document.querySelector("#small-tooltip");
+		this.events();
 
-    }
+	}
 
-    events() {
+	events() {
 
-        this.allTips.forEach((el) => {
-
-			el.addEventListener("mouseenter", (e) => {
-				if(!e.target.matches(this.allTipsSelector)) { return; }
-				const text = e.target.getAttribute('data-tip');
-				if(text) { this.showSmallTooltip(e, text) }
-			});
-			
-			el.addEventListener("mousemove", (e) => {
-				this.moveSmallTooltip(e);
-			});
-			
-			el.addEventListener("mouseleave", () => {
+		addEventListener("mouseover", (e) => {
+			const tip = e.target.closest(this.allTipsSelector);
+			if (tip) {
+				const text = tip.getAttribute('data-tip');
+				if (
+					!this.isIn ||
+					(
+						text &&
+						this.lastText != text
+					)
+				) {
+					this.isIn = true;
+					this.showSmallTooltip(e, text);
+					this.lastText = text;
+				}
+			} else if (this.isIn) {
+				this.isIn = false;
 				this.hideSmallTooltip();
-			});
-			
-			el.addEventListener("click", () => {
-				this.hideSmallTooltip();
-			});
-
+			}
 		});
 
-    }
+		addEventListener("mousemove", (e) => {
+			this.moveSmallTooltip(e);
+		});
 
-    showSmallTooltip(e, text) {
-		this.moveSmallTooltip(e);
-		this.dom.innerHTML = text;
-		this.dom.classList.add("show");
 	}
-	
+
+	showSmallTooltip(e, text) {
+		if (this.isIn) {
+			this.dom.classList.add("show");
+			this.moveSmallTooltip(e);
+			this.dom.innerHTML = text;
+		}
+	}
+
 	hideSmallTooltip() {
 		this.dom.classList.remove("show");
 	}
-	
+
 	moveSmallTooltip(e) {
-		this.dom.style.top = e.pageY;
-		this.dom.style.left = e.pageX;
-		this.dom.style.marginLeft = this.dom.offsetWidth/-2;
+		this.dom.style.top = e.pageY + 'px';
+		this.dom.style.left = e.pageX + 'px';
+		this.dom.style.marginLeft = (this.dom.offsetWidth / -2) + 'px';
 	}
 
-    injectHTML() {
-        document.body.insertAdjacentHTML('beforeend', '<div id="small-tooltip"></div>');
-    }
+	injectHTML() {
+		document.body.insertAdjacentHTML('beforeend', '<div id="small-tooltip"></div>');
+	}
 
 }
 
